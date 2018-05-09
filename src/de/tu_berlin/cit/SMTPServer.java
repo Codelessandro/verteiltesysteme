5360d6ef238e9bdd07d776553aca76923e45f2dd
Christian M <mischok.christian@gmail.com>
2018-05-09 17:39:40 +0200

    created new class SMTPServerState for sending response codes in the corresponding channel to the client

--- src/de/tu_berlin/cit/SMTPServer.java
+++ src/de/tu_berlin/cit/SMTPServer.java
@@ -1,6 +1,6 @@
 package de.tu_berlin.cit;
 
-import SMTPClientState;
+//import SMTPClientState;
 import java.nio.*;
 import java.nio.charset.*;
 import java.nio.file.Files;
@@ -15,93 +15,6 @@ import java.util.*;
 
 
 public class SMTPServer {
-
-	
-		
-//		if (args.length != 2) {
-//			System.out.println("to less arguments");
-//			System.exit(1);
-//		}
-		
-//		String str = "Hello";
-//		byte[] bytes = str.getBytes();
-//		for (byte b: bytes) {
-//			System.out.println(b);
-//		}
-		
-		// String -> bytes
-/*		String s = "Verteilte Systeme";
-		Charset msgCharset = Charset.forName("US-ASCII");
-		byte[] b = s.getBytes(msgCharset);
-		ByteBuffer bb = ByteBuffer.allocate(17);
-		bb.put(b);
-		ByteBuffer bb = ByteBuffer.wrap(b);
-		System.out.println("encoded: " + bb);
-		
-		// bytes -> String
-		CharsetDecoder decoder = msgCharset.newDecoder();
-		CharBuffer charBuf = decoder.decode(bb);
-		String decStr = charBuf.toString();
-		System.out.println("decoded: " + decStr);
-		
-		// Selector for selecting channel registration
-		Selector selector = Selector.open();
-		ServerSocketChannel servSock = ServerSocketChannel.open();
-		servSock.configureBlocking(false);
-		servSock.socket().bind(new InetSocketAddress(1234));
-		servSock.register(selector, SelectionKey.OP_ACCEPT);
-		
-		while(true) {
-			if(selector.select() == 0) {
-				continue;
-			}
-			
-			Set<SelectionKey> selectedKeys = selector.selectedKeys();
-			Iterator<SelectionKey> iter = selectedKeys.iterator();
-			
-			while(iter.hasNext()) {
-				SelectionKey key = iter.next();
-				// check whether the key of the channel accepts new socket connection
-				if(key.isAcceptable()) {
-					ServerSocketChannel sock = (ServerSocketChannel) key.channel();
-					SocketChannel client = sock.accept();
-					client.configureBlocking(false);
-					client.register(selector, SelectionKey.OP_READ | SelectionKey.OP_WRITE);
-				}
-				if(key.isReadable()) {
-					ByteBuffer buf = ByteBuffer.allocate(1024);
-					SocketChannel channel = (SocketChannel) key.channel();
-					channel.read(buf);
-					buf.flip();
-				}
-				
-				if(key.isConnectable()) {
-					
-				}
-				
-				iter.remove();
-			}
-		}
-		
-		
-		
-		
-		
-		
-		
-		
-//		InetSocketAddress address = new InetSocketAddress(args[0], Integer.parseInt(args[1]));
-//		Socket server = null;
-//		try {
-//			server = new Socket(address.getHostName(), address.getPort());
-//		} catch (UnknownHostException e) {
-//			System.err.println("Host does not exist");
-//			e.printStackTrace();
-//		} catch (IOException e) {
-//			System.err.println("Problem during IO operation");
-//			e.printStackTrace();
-//		}
-	*/
 		
 	public static void main(String[] args) throws IOException, CharacterCodingException,
 			UnsupportedCharsetException {
@@ -117,6 +30,7 @@ public class SMTPServer {
 		while (true) {
 			
 			if (selector.select() == 0) {
+				System.out.println("select(): " + selector.select());
 				continue;
 			}
 			
@@ -126,10 +40,13 @@ public class SMTPServer {
 			while (iter.hasNext()) {
 				SelectionKey key = iter.next();
 				
-				/*System.out.println("Server: " + key.toString());*/
-				//System.out.println("Server: " + key.readyOps());
+				/*System.out.println("Server: " + key.toString());
+				System.out.println("Server: " + key.readyOps());*/
 				System.out.println("isWritable: " + key.isWritable());
 				System.out.println("isReadable: " + key.isReadable());
+				System.out.println("isAcceptable: " + key.isAcceptable());
+				
+				
 				
 				if (key.isAcceptable()) {
 					System.out.println("Server: isAccaptable()");
@@ -141,21 +58,36 @@ public class SMTPServer {
 				
 				if (key.isReadable()) {
 					System.out.println("Server: isReadable()");
-					ByteBuffer bb = ByteBuffer.allocate(1024);
-					SocketChannel channel = (SocketChannel) key.channel();
-					channel.read(bb);
-					bb.flip();
-					System.out.println("Buffer: " + bb);
+					
+					ByteBuffer buffer = ByteBuffer.allocate(1024);
+					SocketChannel clientChannel = (SocketChannel) key.channel();
+					clientChannel.read(buffer);
+					buffer.flip();
+					System.out.println("Buffer: " + buffer);
+					String result = new String(buffer.array()).trim();
+					
+					if (result.endsWith("\n\r")) {
+						clientChannel.close();
+					}
 				}
 				
 				if (key.isWritable()) {
 					System.out.println("Server: isWritable()");
 					
-					Charset msgCharset = Charset.forName("US-ASCII");
+					byte responseCode = (byte) 250;
+					
+					ByteBuffer bb = ByteBuffer.allocate(10);
+					bb.put(responseCode);
+					
+					SocketChannel client = (SocketChannel) key.channel();
+					key.attachment()
+					client.write(bb);
+					
+					/*Charset msgCharset = Charset.forName("US-ASCII");
 					CharsetDecoder decoder = msgCharset.newDecoder(); 
 					ByteBuffer buffer = ByteBuffer.allocate(1024);
 					SocketChannel channel = (SocketChannel) key.channel();
-					SMTPClientState state = key.attachment();
+					//SMTPClientState state = key.attachment();
 					channel.read(buffer);
 					System.out.println("capacity: " + buffer.capacity());
 					System.out.println("buffer: " + buffer);
@@ -172,7 +104,7 @@ public class SMTPServer {
 					//File file = Files.createFile(Paths.get("./")).toFile();
 					//System.out.println(file);
 					
-					/*System.out.println("print File: ");
+					System.out.println("print File: ");
 					FileChannel fileChannel = FileChannel.open(file, StandardOpenOption.WRITE);
 					fileChannel.write(buffer);*/
 				}
---
+++ src/de/tu_berlin/cit/SMTPServerState.java
@@ -0,0 +1,10 @@
+package de.tu_berlin.cit;
+
+public class SMTPServerState {
+
+	private int responseCode;
+	
+	public SMTPServerState(int responseCode) {
+		this.responseCode = responseCode;
+	}
+}
