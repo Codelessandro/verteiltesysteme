6570c5c87c04cfa7b634a40e73b819875ec9b39d
Christian M. <mischok.christian@gmail.com>
2018-05-11 13:25:58 +0200

    added objects for endoding and decoding

--- src/de/tu_berlin/cit/SMTPServer.java
+++ src/de/tu_berlin/cit/SMTPServer.java
@@ -71,71 +71,19 @@ public class SMTPServer {
 					buffer.flip();
 					System.out.println("Buffer: " + buffer);
 					
-					/*String result = new String(buffer.array()).trim();
-					
-					if (result.endsWith("\n\r.\n\r")) {
-						clientChannel.close();
-					}*/
 				}
 				
 				if (key.isWritable()) {
 					System.out.println("Server: isWritable()");
 					
 					// write a response code to the client with a hyphen
-					/*SocketChannel client = (SocketChannel) key.channel();
-					byte[] hyphen = new String("250-first line").getBytes(msgCharset);
+					SocketChannel channel = (SocketChannel) key.channel();
+					byte[] hyphen = new String("250-balbalalala\r\n").getBytes(msgCharset);
 					ByteBuffer buffer = ByteBuffer.wrap(hyphen);
-					client.write(buffer);*/
-					
-					// retrieve the state object from the channel
-					/*SocketChannel channel = (SocketChannel) key.channel();
-					SMTPClientState state = (SMTPClientState) key.attachment();
-					System.out.println("client state: " + state);*/
-					
-					// attach a new server state object to the channel
-					SMTPServerState state = new SMTPServerState();
-					state.setMessage(new String("220-service ready").getBytes(msgCharset));
-					key.attach(state);
+					channel.write(buffer);
 					
 				}
 				
-				/*if (key.isWritable()) {
-					System.out.println("Server: isWritable()");
-					
-					byte responseCode = (byte) 250;
-					
-					ByteBuffer bb = ByteBuffer.allocate(10);
-					bb.put(responseCode);
-					
-					SocketChannel client = (SocketChannel) key.channel();
-					key.attachment();
-					client.write(bb);
-					
-					/*Charset msgCharset = Charset.forName("US-ASCII");
-					CharsetDecoder decoder = msgCharset.newDecoder(); 
-					ByteBuffer buffer = ByteBuffer.allocate(1024);
-					SocketChannel channel = (SocketChannel) key.channel();
-					//SMTPClientState state = key.attachment();
-					channel.read(buffer);
-					System.out.println("capacity: " + buffer.capacity());
-					System.out.println("buffer: " + buffer);
-					CharBuffer charBuf = decoder.decode(buffer);
-					System.out.println("capacity: " + charBuf.capacity());
-					System.out.println("position: " + charBuf.position());
-					System.out.println("limit: " + charBuf.limit());
-					//System.out.println(charBuf);
-					
-					//System.out.println("str: " + res);
-					
-					
-					//Path dir = Files.createDirectory(Paths.get("sender"));
-					//File file = Files.createFile(Paths.get("./")).toFile();
-					//System.out.println(file);
-					
-					System.out.println("print File: ");
-					FileChannel fileChannel = FileChannel.open(file, StandardOpenOption.WRITE);
-					fileChannel.write(buffer);
-				}*/
 				
 				iter.remove();
 			}
